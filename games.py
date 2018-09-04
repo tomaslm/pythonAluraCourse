a3dc8b63e5a1908e7410de9ab5470cdc48bf0351
Tomas Labella Menito <tomas.menito@hexagonagriculture.com>
2018-09-04 14:14:03 -0300

    Initial implementation of games

--- games.py
+++ games.py
@@ -1,7 +1,20 @@
 import guess_number
+import hangman
 
 def play():
-    guess_number.play()
+    print("***********************")
+    print("***  Choose a game  ***")
+    print("***                 ***")
+    print("***  (H)angman      ***")
+    print("***  (G)uess number ***")
+    print("***                 ***")
+    print("***********************")
+    
+    option =  input(": ")
+    if option == 'H':
+        guess_number.play()
+    elif option == 'G':
+        hangman.play()
 
 if __name__ == "__main__":
     play();
---
+++ hangman.py
@@ -0,0 +1,8 @@
+def play():
+    print("**********************")
+    print("***Guess the word***")
+    print("**********************")
+
+
+if __name__ == "__main__":
+    play()
